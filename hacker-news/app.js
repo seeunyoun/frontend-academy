6ebcf4c6fe2d8bfb52e8b6ddab27dd0f1186d87a
SloaneY <tpdms717@gmail.com>
2021-06-06 19:47:11 +0900

    ♻️ Refactoring ajax code

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -4,19 +4,20 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false); // 데이터를 오픈한다. boolean값 -> true:비동기 false:동기
-ajax.send(); // 데이터를 가져온다
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response); // 응답값을 객체로 바꾼다 (JSON 데이터만 객체로 바꿀 수 있음)
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL); // 응답값을 객체로 바꾼다 (JSON 데이터만 객체로 바꿀 수 있음)
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", function () {
   // 해쉬가 바뀌면! 함수가 실행되는 이벤트 추가
   const id = location.hash.substr(1); // 해시+id값에서 해시를 제외하는 문법!
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
@@ -24,14 +25,17 @@ window.addEventListener("hashchange", function () {
 });
 
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
+  const div = document.createElement("div");
 
-  a.href = `#${newsFeed[i].id}`; // 해시 뒤에 id값을 같이 불러옴!
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+  </li>
+  `;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
